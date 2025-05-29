4bbf89aa4073d32cc878cb8ba96a88883e869f82
TONNY CHIBAMBO <tonnychibambo@gmail.com>
2025-05-29 15:54:46 +0200

    added a class modules.py
    setting  up the window, title , size as well as the back ground color

--- web scraper modules.py
+++ web scraper modules.py
@@ -8,3 +8,10 @@ from tkinterweb.htmlwidgets import HtmlFrame  # HTML display in Tkinter
 import threading  # Multithreading support
 import logging  # Logging utilities
 from datetime import datetime  # Date and time handling
+
+class WebScraperApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Web Scraper & IP Resolver")
+        self.root.geometry("900x700")
+        self.root.configure(bg="#E0E7FF")  
